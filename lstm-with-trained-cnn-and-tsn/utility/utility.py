800fba7c4525908cf29af1be8593d9d3802bd24d
Angelo Filannino <filannino.angelo@gmail.com>
2019-01-08 22:18:04 +0100

    [exp3] Start working on experiment 3
    Created basic structure

---
+++ lstm-with-trained-cnn-and-tsn/utility/utility.py
@@ -0,0 +1,58 @@
+import csv
+import os
+import numpy as np
+
+from utility.video_object import VideoObject
+
+
+def project_root():
+    return 'PATH TO lstm-with-trained-cnn-and-tsn'
+
+
+def ffmpeg_path():
+    return 'PATH TO ffmpeg bin folder'
+
+
+def retrieve_videoobject_subsets(subsets):
+    videoobject_subsets = []
+    # Loop over the three subsets: train, validation, test
+    for subset in subsets:
+        videoobject_subset = []
+
+        if not os.path.exists(os.path.join(project_root(), 'data', subset + '-set.csv')):
+            videoobject_subsets.append([])
+            continue
+
+        # Open the csv file and retrieve a list of VideoObject
+        with open(os.path.join(project_root(), 'data', subset + '-set.csv'), 'r', newline='\n') as csvfile:
+            csv_reader = csv.reader(csvfile, delimiter=',')
+            next(csv_reader)  # Skip the header
+            for row in csv_reader:
+                videoobject_subset.append(VideoObject(row[0], row[1], row[2]))
+        videoobject_subsets.append(videoobject_subset)
+    return videoobject_subsets
+
+
+def limit_frames_number(frame_list, limit):
+    # Check if requirement are already satisfied
+    if len(frame_list) <= limit:
+        return frame_list
+
+    step = len(frame_list) / limit
+    index = map(lambda x: round(x * step), list(range(limit)))
+
+    # Pick 'size_limit' frames from the frame list and reorder them
+    # new_frame_list = np.random.choice(frame_list, size=limit, replace=False).tolist()
+    # new_frame_list = frame_list[::step]
+    new_frame_list = [frame_list[i] for i in index]
+    new_frame_list.sort()
+    return new_frame_list[:limit]
+
+
+def retrieve_classes():
+    classes = []
+    with open(os.path.join(project_root(), 'data', 'classes.csv'), 'r', newline='\n') as csvfile:
+        csv_reader = csv.reader(csvfile, delimiter=',')
+        for row in csv_reader:
+            classes.append(row[0])
+    return classes
