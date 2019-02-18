f20d4f6d1532ae06feab7bfd04f4066b8007a507
Angelo Filannino <filannino.angelo@gmail.com>
2019-02-18 23:12:34 +0100

    [exp3] update before training

--- lstm-with-trained-cnn-and-tsn/utility/experiment_utilities.py
+++ lstm-with-trained-cnn-and-tsn/utility/experiment_utilities.py
@@ -3,6 +3,7 @@ import random
 import glob
 import numpy as np
 from keras.utils import to_categorical
+from keras.models import Model
 
 from utility.utility import project_root, retrieve_classes
 
@@ -78,17 +79,26 @@ def create_sequence_generator(
 
 
 def create_sequence_of_tuple_generator(
-        subset, batch_size, feature_sequence_length, feature_length, model_flow, model_rgb, flow_feature, number_of_segment):
+        subset, batch_size, feature_sequence_length, feature_length, model_flow, model_rgb, number_of_segment):
 
     classes = retrieve_classes()
     counter = 0
 
-    if flow_feature:
-        folder_suffix = '_flowfeatures'
-        dataset_folder_name = 'UCF-101-flow-features'
-    else:
-        folder_suffix = '_features'
-        dataset_folder_name = 'UCF-101-rgb-features'
+    model_flow = Model(
+        inputs=model_flow.input,
+        outputs=model_flow.get_layer('flow_dense2').output
+    )
+
+    model_rgb = Model(
+        inputs=model_rgb.input,
+        outputs=model_rgb.get_layer('rgb_dense2').output
+    )
+
+
+    flow_folder_suffix = '_flowfeatures'
+    flow_dataset_folder_name = 'UCF-101-flow-features'
+    rgb_folder_suffix = '_features'
+    rgb_dataset_folder_name = 'UCF-101-rgb-features'
 
     while True:  # This is because generator must be infinite loop
         x_batch_rgb = []
@@ -114,24 +124,31 @@ def create_sequence_of_tuple_generator(
             counter = 0
 
         for video in current_batch:
-            feature_filename_pattern = os.path.join(project_root, 'data',
-                                                    dataset_folder_name, video.label, video.label + folder_suffix,
+            rgb_feature_filename_pattern = os.path.join(project_root, 'data',
+                                                    rgb_dataset_folder_name, video.label, video.label + rgb_folder_suffix,
                                                     'v_' + video.label + '_' + video.group + '_' + video.clip + '*.npy')
 
-            segment_features_list = glob.glob(feature_filename_pattern)
+            flow_feature_filename_pattern = os.path.join(project_root, 'data',
+                                                    flow_dataset_folder_name, video.label, video.label + flow_folder_suffix,
+                                                    'v_' + video.label + '_' + video.group + '_' + video.clip + '*.npy')
 
-            if len(segment_features_list) != number_of_segment:
+            rgb_segment_features_list = glob.glob(rgb_feature_filename_pattern)
+            flow_segment_features_list = glob.glob(flow_feature_filename_pattern)
+
+            if len(rgb_segment_features_list) != number_of_segment:
+                raise Exception('A video has a wrong number of feature part. Please check your parameters')
+            if len(flow_segment_features_list) != number_of_segment:
                 raise Exception('A video has a wrong number of feature part. Please check your parameters')
 
             predictions_list_rgb = []
             predictions_list_flow = []
-            for segment_feature in segment_features_list:
+            for rgb_segment_feature in rgb_segment_features_list:
 
-                if not os.path.isfile(segment_feature):
+                if not os.path.isfile(rgb_segment_feature):
                     raise Exception('Feature not found! You have to create all the features!')
 
                 # Retrieving features
-                features_sequence = np.load(segment_feature)
+                features_sequence = np.load(rgb_segment_feature)
                 length = len(features_sequence)
                 if length > feature_sequence_length:
                     raise Exception('Some sequences of features are too long! Please compute them again!')
@@ -143,13 +160,31 @@ def create_sequence_of_tuple_generator(
                     features_sequence = features_sequence.reshape((feature_sequence_length, feature_length))
 
                 prediction_rgb = model_rgb.predict(features_sequence, batch_size=1, verbose=1)
-                prediction_flow = model_flow.predict(features_sequence, batch_size=1, verbose=1)
                 predictions_list_rgb.append(prediction_rgb)
+
+            for flow_segment_feature in flow_segment_features_list:
+
+                if not os.path.isfile(flow_segment_feature):
+                    raise Exception('Feature not found! You have to create all the features!')
+
+                # Retrieving features
+                features_sequence = np.load(flow_segment_feature)
+                length = len(features_sequence)
+                if length > feature_sequence_length:
+                    raise Exception('Some sequences of features are too long! Please compute them again!')
+                # Zero padding if size is different from size_limit
+                elif length < feature_sequence_length:
+                    features_sequence = np.append(features_sequence,
+                                                  np.zeros((feature_sequence_length - length, feature_length),
+                                                           dtype='float32'))
+                    features_sequence = features_sequence.reshape((feature_sequence_length, feature_length))
+
+                prediction_flow = model_flow.predict(features_sequence, batch_size=1, verbose=1)
                 predictions_list_flow.append(prediction_flow)
 
             x_batch_rgb.append(predictions_list_rgb)
             x_batch_flow.append(predictions_list_flow)
             y_batch.append(to_categorical(classes.index(video.label), len(classes)))
-            
+
         x_batch.append([x_batch_rgb, x_batch_flow])
         yield np.array(x_batch), np.array(y_batch)
