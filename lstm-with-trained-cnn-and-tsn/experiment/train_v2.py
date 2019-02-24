86e270780623b0cd023910071c302ef5c9373546
Angelo Filannino <filannino.angelo@gmail.com>
2019-02-24 09:40:11 +0100

    [exp3] update for training

--- lstm-with-trained-cnn-and-tsn/experiment/train_v2.py
+++ lstm-with-trained-cnn-and-tsn/experiment/train_v2.py
@@ -58,7 +58,7 @@ def create_model():
     flow_dense_final = Dense(classes_size, activation='softmax', name='flow_dense_final')(flow_dropout)
 
     model = Model(inputs=[rgb_input, flow_input], outputs=[rgb_dense_final, flow_dense_final])
-    model.load_weights(os.path.join(project_root, 'data', 'result', 'model_weights', 'tsn_model-010-0.588-0.103.hdf5'))
+    model.load_weights(os.path.join(project_root, 'data', 'result', 'model_weights', 'tsn_model-010-0.683-0.148.hdf5'))
 
     # Hyper parameters
     loss = 'categorical_crossentropy'
